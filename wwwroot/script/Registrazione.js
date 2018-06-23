4ef3d16d626276c29587cd6b89612194a47c42a6
Francesco <marnelli90@outlook.it>
2018-06-23 18:36:29 +0200

    cazzo duro

---
+++ wwwroot/script/Registrazione.js
@@ -0,0 +1,17 @@
+﻿function ShowForm(){
+    var radio_privato = document.getElementById("radio-privato");
+    var radio_azienda = document.getElementById("radio-azienda");
+
+    var privato = document.getElementById("privato");
+    var azienda = document.getElementById("azienda");
+
+    if (radio_privato.checked) {
+        privato.style.display = 'block';
+        azienda.style.display = 'none';
+    }
+
+    if (radio_azienda.checked) {
+        privato.style.display = 'none';
+        azienda.style.display = 'block';
+    }
+}
